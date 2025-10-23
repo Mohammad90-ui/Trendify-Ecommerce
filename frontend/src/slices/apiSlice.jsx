c6d3934c41c0a733dae5fec0ffce4a67bbf4e454
Mohammad Hasan <mohammadhasan707053@gmail.com>
2025-10-23 22:41:13 +0530

    updated URL

--- frontend/src/slices/apiSlice.jsx
+++ frontend/src/slices/apiSlice.jsx
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { logout } from './authSlice';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'http://localhost:5000',
+  baseUrl: import.meta.env.VITE_API_URL,
   credentials: 'include',
 });
 
