a49c3003260deea61ca95847a6d97763962004ff
Mohammad Hasan <mohammadhasan707053@gmail.com>
2025-10-24 14:52:44 +0530

    origin updated

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 
 // CORS middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://trendify-ecommerce-nu.vercel.app'],
   credentials: true
 }));
 
